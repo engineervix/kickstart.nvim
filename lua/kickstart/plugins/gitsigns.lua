4077347226b3844baf24de7d33544473bad08c79
Victor Miti <victor.miti@torchbox.com>
2026-04-22 16:15:37 +0200

    feat(gitsigns): enable current line blame by default

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -11,6 +11,7 @@ return {
     ---@type Gitsigns.Config
     ---@diagnostic disable-next-line: missing-fields
     opts = {
+      current_line_blame = true,
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
