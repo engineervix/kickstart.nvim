e59b9a65a89908281f014ae9f8e1b8a376c71f6b
Victor Miti <victormiti@umusebo.com>
2026-05-28 02:33:11 +0200

    feat(navigation): add harpoon for pinned file quick-access
    Mark files with <leader>ha, jump to them with <leader>1-4.
    Pins are scoped per git branch via mark_branch = true.

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,23 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+  {
+    'ThePrimeagen/harpoon',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+      { '<leader>ha', function() require('harpoon.mark').add_file() end, desc = '[H]arpoon: [a]dd file' },
+      { '<leader>hh', function() require('harpoon.ui').toggle_quick_menu() end, desc = '[H]arpoon: toggle menu' },
+      { '<leader>1', function() require('harpoon.ui').nav_file(1) end, desc = 'Harpoon: go to file 1' },
+      { '<leader>2', function() require('harpoon.ui').nav_file(2) end, desc = 'Harpoon: go to file 2' },
+      { '<leader>3', function() require('harpoon.ui').nav_file(3) end, desc = 'Harpoon: go to file 3' },
+      { '<leader>4', function() require('harpoon.ui').nav_file(4) end, desc = 'Harpoon: go to file 4' },
+    },
+    opts = {
+      global_settings = {
+        save_on_toggle = true,
+        save_on_change = true,
+        mark_branch = true,
+      },
+    },
+  },
+}
