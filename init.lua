92db5ebe4807f3a0845291b93c8d78df2d8e7d01
Victor Miti <victormiti@umusebo.com>
2026-05-28 02:22:11 +0200

    feat(navigation): add flash.nvim for fast jump navigation
    Adds flash.nvim with standard keymaps (s/S/r/R/<c-s>).
    Integrates with Telescope: <c-s> in insert and s in normal mode
    jump to results with flash labels.

--- init.lua
+++ init.lua
@@ -390,8 +390,44 @@ require('lazy').setup({
         --
         defaults = {
           mappings = {
-            i = { ['<c-t>'] = require('trouble.sources.telescope').open },
-            n = { ['<c-t>'] = require('trouble.sources.telescope').open },
+            i = {
+              ['<c-t>'] = require('trouble.sources.telescope').open,
+              ['<c-s>'] = function(prompt_bufnr)
+                require('flash').jump {
+                  pattern = '^',
+                  label = { after = { 0, 0 } },
+                  search = {
+                    mode = 'search',
+                    exclude = {
+                      function(win) return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= 'TelescopeResults' end,
+                    },
+                  },
+                  action = function(match)
+                    local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+                    picker:set_selection(match.pos[1] - 1)
+                  end,
+                }
+              end,
+            },
+            n = {
+              ['<c-t>'] = require('trouble.sources.telescope').open,
+              s = function(prompt_bufnr)
+                require('flash').jump {
+                  pattern = '^',
+                  label = { after = { 0, 0 } },
+                  search = {
+                    mode = 'search',
+                    exclude = {
+                      function(win) return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= 'TelescopeResults' end,
+                    },
+                  },
+                  action = function(match)
+                    local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+                    picker:set_selection(match.pos[1] - 1)
+                  end,
+                }
+              end,
+            },
           },
         },
         extensions = {
---
+++ lua/custom/plugins/flash.lua
@@ -0,0 +1,17 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      { 's', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end, desc = 'Flash: jump' },
+      { 'S', mode = { 'n', 'x', 'o' }, function() require('flash').treesitter() end, desc = 'Flash: treesitter node' },
+      { 'r', mode = 'o', function() require('flash').remote() end, desc = 'Flash: remote' },
+      { 'R', mode = { 'o', 'x' }, function() require('flash').treesitter_search() end, desc = 'Flash: treesitter search' },
+      { '<c-s>', mode = 'c', function() require('flash').toggle() end, desc = 'Flash: toggle search labels' },
+    },
+  },
+}
